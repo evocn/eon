82f089f052e2cd22baee330fcd048457e69d77c7
Alex Hartford <alexanderhartford@gmail.com>
2024-03-14 21:36:30 -0700

    workin out the dough

--- everett.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd ../../Code/everett/bin
-start remedybg everett.rdbg
-start wt.exe -d C:/Users/alexa/Code/everett/bin
-start gvim -c "simalt ~x" ../src/main.jai
---
+++ work.bat
@@ -0,0 +1,4 @@
+@echo off
+start wt.exe -d %~dp0
+start remedybg bin\everett.rdbg
+start gvim build.jai
